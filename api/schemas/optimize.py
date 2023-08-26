1bc1b284dc6ba8d44e2dc118ff9af394dd6016e7
satoshi19989 <3104monogotueeko@gmail.com>
2023-08-26 09:09:17 +0900

    (update) クラウドデプロイ処理を実装

--- api/schemas/optimize.py
+++ api/schemas/optimize.py
@@ -3,13 +3,24 @@ from pydantic import Field, field_validator, conint
 from pydantic import BaseModel as PydanticBaseModel
 import typing as tp
 import pandas as pd
+from dataclasses import dataclass
 
 class BaseModel(PydanticBaseModel):
     class Config:
         arbitrary_types_allowed = True
 
+# TODO :暫定で以下のクラスを使用。本当は、pydantic.BaseModelを使用したい。
+@dataclass
+class RequestedSchedule:
+    n_member_day:int
+    upload_file: tp.Optional[UploadFile]
 
-class RequestedSchedule(BaseModel):
+    def generate_df(self) -> pd.DataFrame:
+        return pd.read_csv(self.upload_file.file, index_col=0, header=0)
+    
+
+# TODO :本当は以下のクラスのようなバリデーション可能な実装としたいが、うまくいかない。
+class RequestedScheduleValidatable(BaseModel):
     n_member_day:conint(strict=True, ge=1)
     upload_file: tp.Optional[UploadFile]
 
